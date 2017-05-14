5f3dbddcb8923c276b9cf0650174842d45636335
Michael Vial <michaelvial@gmail.com>
2017-05-14 13:12:31 +0200

    Fix gradient color overflow

--- demo/js/colors.js
+++ demo/js/colors.js
@@ -22,9 +22,9 @@ export function getGetGradientColorForRatio(color1, color2, ratio) {
   color1 = color1.toString(16)
   color2 = color2.toString(16)
 
-  const r = Math.ceil(parseInt(color2.substring(0,2), 16) * ratio + parseInt(color1.substring(0,2), 16) * (1-ratio));
-  const g = Math.ceil(parseInt(color2.substring(2,4), 16) * ratio + parseInt(color1.substring(2,4), 16) * (1-ratio));
-  const b = Math.ceil(parseInt(color2.substring(4,6), 16) * ratio + parseInt(color1.substring(4,6), 16) * (1-ratio));
+  const r = Math.floor(parseInt(color2.substring(0,2), 16) * ratio + parseInt(color1.substring(0,2), 16) * (1-ratio));
+  const g = Math.floor(parseInt(color2.substring(2,4), 16) * ratio + parseInt(color1.substring(2,4), 16) * (1-ratio));
+  const b = Math.floor(parseInt(color2.substring(4,6), 16) * ratio + parseInt(color1.substring(4,6), 16) * (1-ratio));
 
   return parseInt(hex(r) + hex(g) + hex(b), 16)
 }
